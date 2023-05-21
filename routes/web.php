84ffd65dd33974107ee434036a85b508966b9c4f
Adamibnualfatahh <adamibnu012@gmail.com>
2023-05-21 09:40:01 +0700

    feat: setup spatie permission

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('admin', function () {
+    return 'Admin';
+})->middleware(['auth', 'role:admin']);
+
 require __DIR__.'/auth.php';
