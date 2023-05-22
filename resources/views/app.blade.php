7caf37bcf14964b6b0a21455b8dac74ee2204141
Adamibnualfatahh <adamibnu012@gmail.com>
2023-05-22 10:59:27 +0700

    feat: create login page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,16 +7,16 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+       <style>
+            @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
+       </style>
         <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-poppins">
         @inertia
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', 'prototype/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -38,5 +31,11 @@ Route::middleware('auth')->group(function () {
 Route::get('admin', function () {
     return 'Admin';
 })->middleware(['auth', 'role:admin']);
+
+Route::prefix('prototype')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    });
+});
 
 require __DIR__.'/auth.php';
