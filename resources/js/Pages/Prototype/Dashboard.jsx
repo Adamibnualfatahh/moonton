02c237fec229fd22803025964d2f1360be246b2d
adamibnu <adamibnu157@gmail.com>
2023-06-09 15:00:28 +0700

    feat: create movie dashboard

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,6 +1,8 @@
 import Authenticated from "@/Layouts/Authenticated/Index.jsx";
 import Flickity from "react-flickity-component";
 import {Head} from "@inertiajs/react";
+import FeaturedMovie from "@/Components/FeaturedMovie.jsx";
+import MovieCard from "@/Components/MovieCard.jsx";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -12,40 +14,39 @@ export default function Dashboard() {
         "prevNextButtons": false,
         "draggable": ">1"
     }
-	return <Authenticated>
+    return <Authenticated>
         <Head title={"Dashboard"}>
-            <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+            <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css"/>
         </Head>
-        <div>
             <div className="font-semibold text-[22px] text-black mb-4">Featured Movies</div>
             <Flickity className="gap-[30px]" options={flickityOptions}>
-
-                {[1,2,3,4].map(i => (
-                    <div className="absolute overflow-hidden group mr-[30px]" key={i}>
-                        <img src="/images/featured-2.png"
-                             className="object-cover rounded-[30px] w-[520px] h-[340px]" alt=""/>
-                        <div className="rating absolute top-0 left-0">
-                            <div className="p-[30px] flex items-center gap-1">
-                                <img src="/icons/ic_star.svg" alt=""/>
-                                <span className="text-sm font-medium text-white mt-1">4.2/5.0</span>
-                            </div>
-                        </div>
-                        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                                rounded-br-[28px] flex justify-between items-center px-7 h-[130px]">
-                            <div>
-                                <div className="font-medium text-[22px] text-white">Despicable Me 2</div>
-                                <p className="mb-0 text-white text-sm font-light">Action • Adventure</p>
-                            </div>
-                            <div
-                                className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
-                                <img src="/icons/ic_play.svg" width="50" alt=""/>
-                            </div>
-                        </div>
-                        <a href="watching.html" className="inset-0 absolute z-50"></a>
-                    </div>
+                {[1, 2, 3, 4].map(i => (
+                    <FeaturedMovie
+                        key={i}
+                        slug="Test"
+                        name={"Test " + i}
+                        category="Test"
+                        thumbnail={"https://picsum.photos/520/340?random=" + i}
+                        rating={i}
+                    />
                 ))}
-
             </Flickity>
-        </div>
+
+           <div className="mt-[50px]">
+               <div className="font-semibold text-[22px] text-black mb-4">Browse</div>
+               <Flickity className="gap-[30px]" options={flickityOptions}>
+                     {[1, 2, 3, 4, 5, 6].map(i => (
+                        <MovieCard
+                            key={i}
+                            slug="Test"
+                            name={"Test " + i}
+                            category="Test"
+                            thumbnail={"https://picsum.photos/520/340?random=" + (i + i)}
+                            rating={i}
+                        />
+                    ))}
+               </Flickity>
+           </div>
+
     </Authenticated>;
 }
